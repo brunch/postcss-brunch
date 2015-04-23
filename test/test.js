38097cb44178720067710fea165fbba8afbfa491
Vincent De Oliveira <vincent@iamvdo.me>
2015-04-23 12:17:51 +0200

    Test rewrited module

--- test/test.js
+++ test/test.js
@@ -1,16 +1,58 @@
 var Plugin = require("../lib");
+var fs = require('fs');
 var should = require('should');
 
 describe('Plugin', function () {
 
-  var plugin;
+  var plugin, config;
 
   beforeEach(function() {
-    plugin = new Plugin({});
+    config = {};
+    plugin = new Plugin(config);
   });
 
-  it("should be an object", function() {
+  it('should be an object', function() {
     plugin.should.be.ok;
+  });
+
+  it('compile', function (done) {
+    var css = 'a{a:a}';
+    plugin.compile({data: css}, function (err, data) {
+      data.should.be.eql(css);
+      done();
+    });
+  });
+
+  describe('Options', function () {
+
+    beforeEach(function() {
+      config = {
+        plugins: {
+          postcss: {
+            processors: [
+              require('autoprefixer-core')({browsers: 'last 99 versions'}),
+              require('css-mqpacker'),
+              require('csswring')
+            ]
+          }
+        }
+      };
+      plugin = new Plugin(config);
+    });
+
+    it('uses processors', function() {
+      plugin.processors.should.be.an.instanceof(Array).with.lengthOf(3);
+    });
+
+    it('compile with options', function (done) {
+      var css = fs.readFileSync('test/fixtures/sample.css', 'utf-8');
+      var expected = fs.readFileSync('test/fixtures/sample.out.css', 'utf-8');
+      plugin.compile({data: css}, function (err, data) {
+        data.should.be.eql(expected);
+        done();
+      });
+    });
+
   });
 
 });
