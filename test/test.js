65d4eaa205129e7783ed775fc291a95d29254403
Matthias Winkelmann <m@matthi.coffee>
2016-08-16 16:50:08 +0200

    Fixes tests – they did not actually fail even when failing, Also updates should and progeny.

--- test/test.js
+++ test/test.js
@@ -2,6 +2,27 @@
 const Plugin = require('../lib');
 const fs = require('fs');
 const should = require('should');
+const Mocha = require('mocha');
+
+describe ('Tests', () => {
+   const blankOpts = { reporter: function(){} }; // no output
+   it('should fail', done => {
+      const failingTest = new Mocha.Test('failing Test', () => {
+         const data = fs.readFileSync('test/fixtures/sample.css', 'utf-8');
+         const expected = "this is actually not expected";
+         return plugin.process({data}).then(actual => {
+            actual.data.should.eql(expected);
+         });
+      });
+      const mocha = new Mocha(blankOpts);
+      mocha.suite.addTest(failingTest);
+      mocha.run(failures => {
+         failures.should.equal(1);
+         done();
+      })
+   });
+});
+
 
 describe('Plugin', () => {
 
@@ -28,22 +49,22 @@ describe('Plugin', () => {
   });
 
   it('uses processors', () => {
-    plugin.processors.should.be.an.Array.with.lengthOf(3);
+    plugin.processors.should.be.an.Array().with.length(3);
   });
 
   it('compile', () => {
     const data = 'a{a:a}';
-    return plugin.compile({data}, file => {
+    return plugin.compile({data}).then(file => {
       file.data.should.be.eql(data);
     });
   });
 
   it('compile with options', () => {
-    const data = fs.readFileSync('test/fixtures/sample.css', 'utf-8');
-    const expected = fs.readFileSync('test/fixtures/sample.out.css', 'utf-8');
-    return plugin.compile({data}, actual => {
-      actual.data.should.be.eql(expected);
-    });
+     const data = fs.readFileSync('test/fixtures/sample.css', 'utf-8');
+     const expected = fs.readFileSync('test/fixtures/sample.out.css', 'utf-8');
+     return plugin.compile({data}).then(actual => {
+       actual.data.should.eql(expected);
+     });
   });
 
   it('compile with sourcemaps', () => {
@@ -55,7 +76,7 @@ describe('Plugin', () => {
       mappings: 'AAKA,QACE,oBAAc,AAAd,qBAAc,AAAd,iBAAc,AAAd,oBAAc,AAAd,YAAc,CACf,AAPD,cACE,GACE,UAAY,CACb,AAMD,GACE,UAAY,CACb,CAPF',
       file: 'sample.css'
     };
-    return plugin.compile({data, path: 'test/fixtures/sample.css'}, file => {
+    return plugin.compile({data, path: 'test/fixtures/sample.css'}).then(file => {
       file.map.should.be.eql(map);
     });
   });
